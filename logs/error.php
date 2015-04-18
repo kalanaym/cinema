d086340210579fdb697543d551a0b658bed478d2
kalanaym <kalanaym@kalanaym-Satellite-L655.(none)>
2015-04-18 02:52:57 -0300

    Commit inicial proyecto en Joomla

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#<?php die('Forbidden.'); ?>
+#Date: 2015-04-18 05:20:14 UTC
+#Software: Joomla Platform 11.1 Stable+Modified [ Ember ] 01-Jun-2011 06:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2015-04-18	05:20:14	INFO	127.0.0.1	Joomla FAILURE: 	Invalid password
+2015-04-18	05:20:30	INFO	127.0.0.1	Joomla FAILURE: 	User does not exist
